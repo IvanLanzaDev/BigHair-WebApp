f7ec20d1042111f0e5655ecf8052cd6ae1c5d995
IvanLanzaDev <ivanlanza@flatpixel.com.br>
2020-03-25 14:53:04 -0300

    Developer Beta v1.0

--- components/content-index.php
+++ components/content-index.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+
+
+    <!-- MOBILE -->
+    <div class="container-mob d-block d-md-none">
     <div class="mt-4">
         <div class="title-service">
             <p class="lead big-hair-color-1"><small>Unhas</small></p>
@@ -157,5 +161,79 @@
             </div>
         </div>
     </div>
+    </div>
+    <!-- END MOBILE -->
+
+<!-- -------------------------------- -------------------------------- -------------------------------- -------------------------------- --> 
+
+
+    <!-- DESKTOP -->
+
+    <div class="row">
+        <!-- SLIDER -->
+        <div class="container-fluid d-none d-md-block">
+            <div id="carouselExample col-12" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../assets/imgs/slider-example.png"  class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../assets/imgs/slider-example.png" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../assets/imgs/slider-example.png" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+        <!-- END SLIDER -->
+    </div>
+
+    <div class="container text-center mt-4">
+        <!-- SERVICE ICONS -->
+        <h3 class="big-hair-color-1"> Cabelos e serviços de beleza </h3>
+        <div class="d-flex my-4 flex-nowrap justify-content-around">
+            <div class="row">
+               <a href="#" class="default-link">
+                    <div class="service-item px-4 pt-4 pb-1 bg-light border-radius-default mr-3">
+                        <img src="../assets/icons/hair-dryer.svg">
+                        <p class="mt-3 big-hair-color-1">Cabelos</p>
+                    </div>
+                </a>
+                <a href="#" class="default-link">
+                    <div class="px-4 pt-4 pb-1 bg-light border-radius-default mr-3">
+                        <img src="../assets/icons/hair-tint.svg">
+                        <p class="mt-3 big-hair-color-1">Coloração</p>
+                    </div>
+                </a>
+                <a href="#" class="default-link">
+                    <div class="px-4 pt-4 pb-1 bg-light border-radius-default mr-3">
+                        <img src="../assets/icons/hand-icon.svg">
+                        <p class="mt-3 big-hair-color-1">Unhas</p>
+                    </div>
+                </a>
+                <a href="#" class="default-link">
+                    <div class="px-4 pt-4 pb-1 bg-light border-radius-default mr-3">
+                        <img src="../assets/icons/body-icon.svg">
+                        <p class="mt-3 big-hair-color-1">Depilação</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <!-- END SERVICE ICONS -->
+
+        <!-- SERVICE -->
+
+        <div class="row d-flex justify-content-between mt-4 mb-4">
+            <h4 class="big-hair-color-1 mt-2">Unhas</h4>
+            <a href="#" class="badge badge-primary big-hair-bg-1 pt-3 pb-3 px-2">Ver tudo</a>
+        </div>
+
+        <!-- END SERVICE -->
+
+    </div>
+
+    <!-- END DESKTOP -->
 </body>
 </html>
--- components/header.php
+++ components/header.php
@@ -6,7 +6,8 @@
 </head>
 <body>
 
-    <div class="container-fluid">
+    <!-- MOBILE -->
+    <div class="container-fluid d-block d-md-none">
        <div class="row">
             <div class="col-10">
                 <p class="lead title-header text-center mt-3 ml-5">BigHair</p>
@@ -16,7 +17,7 @@
             </div>
        </div>
     </div>
-    <div class="container">
+    <div class="container d-block d-md-none">
         <form method="get">
             <div class="form-row text-center">
                 <div class="col-12">
@@ -25,8 +26,50 @@
                         <i class="fas fa-search big-hair-color-2"></i>
                     </button>
                 </div>
-                
+                </div>
         </form>
     </div>
+    <!-- END MOBILE -->
+
+
+<!-- -------------------------------- -------------------------------- -------------------------------- -------------------------------- --> 
+
+
+    <!-- DESKTOP -->
+
+    <nav class="navbar navbar-expand-lg navbar-light big-hair-bg-3 d-none d-md-flex">
+  <a class="navbar-brand" href="#"><img src="../assets/imgs/logo-header-desktop.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle big-hair-color-1" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categorias
+        </a>
+        <div class="dropdown-menu big-hair-color-1" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item big-hair-color-1" href="#">Categoria 1</a>
+          <a class="dropdown-item big-hair-color-1" href="#">Categoria 2</a>
+          <a class="dropdown-item big-hair-color-1" href="#">Categoria 3</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link big-hair-color-1" href="#">Sobre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link big-hair-color-1" href="#">Contato</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      
+      <button class="btn btn-bighair1 mr-3 my-2 my-sm-0" type="submit">Entrar</button>
+      <button class="btn btn-light my-2 my-sm-0" type="submit">Criar Conta</button>
+    </form>
+  </div>
+</nav>
+
+    <!-- END DESKTOP -->
 </body>
 </html>
